748cc9f67ea8db01de01efed987784974cfb5a6b
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2025-02-04 17:37:42 -0300

    ajuste layout

--- cassino/settings.py
+++ cassino/settings.py
@@ -93,6 +93,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
+LANGUAGE_CODE = 'pt-br'
+
 TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
