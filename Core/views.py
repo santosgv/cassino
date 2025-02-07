d0f87a69f1933764031a4049f7ccd540c426e897
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2025-02-07 15:59:57 -0300

    meta tag, e probabilidades

--- Core/views.py
+++ Core/views.py
@@ -32,15 +32,19 @@ def spin(request):
 
     # Probabilidades por nível (quanto maior o nível, maior a chance de ganhar os maiores prêmios)
     level_weights = {
-        1: [95, 4, 0.5, 0.5, 0.5, 0.5, 0.5],      # Nível 1: Muitas chances de ganhar prêmios pequenos
-        2: [5, 90, 3, 1, 0.5, 0.5, 0.5],          # Nível 2: 90% de chances de 5x, poucos prêmios maiores  
-        3: [30, 30, 15, 10, 7, 5, 3],             # Nível 3: Equilibrado
-        4: [20, 25, 15, 10, 10, 10, 10],          # Nível 4: Difícil ganhar qualquer coisa além de 2x e 5x  
+        1: [80, 15, 3, 1, 0.5, 0.3, 0.2],      # Nível 1: Muitas chances de ganhar prêmios pequenos
+        2: [70, 20, 5, 3, 1, 0.5, 0.5],          # Nível 2: 90% de chances de 5x, poucos prêmios maiores  
+        3: [50, 30, 10, 5, 3, 1.5, 0.5],             # Nível 3: Equilibrado
+        4: [30, 30, 20, 10, 5, 3, 2],          # Nível 4: Difícil ganhar qualquer coisa além de 2x e 5x  
         5: [20, 25, 15, 10, 10, 10, 10],          # Nível 5: Mais chances de ganhar os prêmios altos
     }
 
-    # Escolher pesos baseados no nível do usuário
-    user_level = min(user_credit.level, 5)  # Máximo nível 5
+
+    # Ajustar nível de dificuldade baseado nos créditos e saldo
+    if user_credit.credits > 100 or user_credit.balance > 100:
+        user_level = random.choice([4, 5])  # Se tem mais de 100 créditos, joga nos níveis mais difíceis
+    else:
+        user_level = min(user_credit.level, 5)  # Máximo nível 5
 
     weights = level_weights[user_level]
 
@@ -48,6 +52,7 @@ def spin(request):
     results = random.choices(emojis, weights=weights, k=3)
 
     print(results)
+    print(user_level)
 
     # Verificar se há um ganhador e aplicar o multiplicador
     if len(set(results)) == 1:  # Se todos os símbolos forem iguais
@@ -62,15 +67,17 @@ def spin(request):
             '7️⃣': 500,
         }
         multiplier = multipliers.get(symbol, 0)  # Obtém o multiplicador ou 0 se não encontrado
-        credits_won = multiplier  # Créditos ganhos (baseado no multiplicador)
+        credits_won = (user_credit.credits + 1) * (multiplier)  # Créditos ganhos (baseado no multiplicador)
         user_credit.credits += credits_won  # Adiciona os créditos ganhos ao saldo
+
+        print('creditos ganho', credits_won)
         
         # Aumentar o nível se ganhou
         if user_credit.level < 5:
             user_credit.level += 1
 
         user_credit.save()
-        message = f"Você ganhou +{credits_won}"
+        message = f"Você ganhou x{multiplier}"
     else:
         message = "Tente novamente!"
 
