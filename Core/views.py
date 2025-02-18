66f908e5d4509a7f5bbea79c910ea5f74ead4425
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2025-02-18 17:25:41 -0300

    nova home

--- Core/views.py
+++ Core/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from .models import UserCredit,TransactionHistory
 from accounts.models import Withdrawal
 from django.contrib import messages
-import mercadopago
+#import mercadopago
 from decimal import Decimal
 from django.conf import settings
 from django.core.paginator import Paginator
